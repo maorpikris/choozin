03f90bb4accf34cb19f1d7e5ec5bde6cf0db4dc3
maorpikris <maorp1111@gmail.com>
2019-08-06 16:27:31 +0300

    Login screen ui - version one - just XML

--- app/src/main/java/com/choozin/infra/MainActivity.kt
+++ app/src/main/java/com/choozin/infra/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.choozin
+package com.choozin.infra
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.choozin.R
 
 class MainActivity : AppCompatActivity() {
 
