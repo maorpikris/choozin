c430536b898137f5b45527907d2e42b5476b1002
maorpikris <maorp1111@gmail.com>
2019-08-12 14:46:54 +0300

    + Register screen + After register screen + Post item

---
+++ app/src/main/java/com/choozin/infra/AfterRegestirationActivity.kt
@@ -0,0 +1,13 @@
+package com.choozin.infra
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.choozin.R
+
+class AfterRegestirationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.post_item)
+    }
+}
